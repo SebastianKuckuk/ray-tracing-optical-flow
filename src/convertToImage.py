a5dc79f1bd8f8bca92fe599ad7a86b600f70a4b0
Sebastian Kuckuk <sebastian.kuckuk@fau.de>
2023-10-27 15:44:54 +0200

    add configurable scaling modes to image mapping script

--- src/convertToImage.py
+++ src/convertToImage.py
@@ -8,6 +8,9 @@ parser.add_argument("--input", type=str, required=True, help="input raw data fil
 parser.add_argument("--output", type=str, required=True, help="output image file path")
 parser.add_argument("--width", type=int, default=1024, help="image width")
 parser.add_argument("--height", type=int, default=1024, help="image height")
+parser.add_argument("--scale", type=str, default='off', help="scaling mode {off, fixed, auto, symAuto, absFixed, absAuto}")
+parser.add_argument("--scaleMin", type=float, default=0, help="minimum value for scaling")
+parser.add_argument("--scaleMax", type=float, default=1, help="maximum value for scaling")
 args = parser.parse_args()
 
 # Read raw binary data from the input file
@@ -19,14 +22,36 @@ num_color_channels = 3  # Assuming 3 channels for RGB (Red, Green, Blue)
 
 # Convert the raw data to a NumPy array of bytes
 data_array = np.frombuffer(raw_data, dtype=np.float32)
-min, max = min(data_array), max(data_array)
-scale = (max - min)
-if 0 == scale:
-    scale = 1
-data_array = (data_array - min) / scale
 
 # Reshape the data array to match the image dimensions and channels
-data_array = data_array.reshape((args.height, args.width, num_color_channels))
+data_array = data_array.reshape((args.height, args.width, num_color_channels)).copy()
+
+# scale if enabled
+if 'off' == args.scale:
+    pass  # nothing to do
+elif 'fixed' == args.scale:
+    print(args.scaleMin, args.scaleMax)
+    data_array = (data_array - args.scaleMin) / (args.scaleMax - args.scaleMin)
+elif 'auto' == args.scale:
+    min_val, max_val = data_array.min(), data_array.max()
+    print(min_val, max_val)
+    scale = (max_val - min_val)
+    data_array = (data_array - min_val) / (scale if scale != 0 else 1)
+elif 'symAuto' == args.scale:
+    extent = max(-data_array.min(), data_array.max())
+    print(extent)
+    scale = 2 * extent
+    data_array = (data_array + extent) / (scale if scale != 0 else 1)
+elif 'absFixed' == args.scale:
+    scale = args.scaleMax
+    print(scale)
+    data_array = abs(data_array) / (scale if scale != 0 else 1)
+elif 'absAuto' == args.scale:
+    scale = abs(data_array).max()
+    print(scale)
+    data_array = abs(data_array) / (scale if scale != 0 else 1)
+else:
+    raise Exception(f'Invalid scaling mode {args.scale}')
 
 # Create an image from the data array
 plt.imshow(data_array)
